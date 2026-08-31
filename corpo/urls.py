a4f58db6f99847af1b31fabf61c962db0fb8d696
Suporte <suporte@devcyber.com>
2026-08-30 23:12:16 -0300

    nome do vinculo em despesas

--- corpo/urls.py
+++ corpo/urls.py
@@ -47,7 +47,7 @@ urlpatterns = [
     url(r'despesas/', views.despesas, name="despesas"),
     url(r'nova_despesa/(?P<cod>[0-9]+)/', views.nova_despesa, name="nova_despesa"),
     url(r'pre_nova_despesa/', views.pre_nova_despesa, name="pre_nova_despesa"),
-    url(r'filtro_despesas/', views.filtro_despesas, name="filtro_despesas"),
+    url(r'filtro_contas_pagar/', views.filtro_contas_pagar, name="filtro_contas_pagar"),
     #geral
     url(r'painel/', views.painel, name="painel"),
     url(r'sair/', views.sair, name="sair"),
--- corpo/views.py
+++ corpo/views.py
@@ -149,7 +149,7 @@ def nova_conta_receber(request):
 ###########################
 
 @login_required
-def filtro_despesas(request):
+def filtro_contas_pagar(request):
     localizar_ip = LocationService()
     resultado = localizar_ip.get_location(str(request.META.get('REMOTE_ADDR')))
     Logs(usuario=request.user, data_hora=datetime.now(), dados_post='', dados_pc_acesso=request.META.get('HTTP_USER_AGENT', ''), ip=request.META.get('REMOTE_ADDR'), tipo_acesso=request.method, pagina=request.path, endereco=resultado).save()
