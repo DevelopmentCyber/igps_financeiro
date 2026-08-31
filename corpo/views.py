15962bb4be0f4ad257b7da7b70c8b3325375c4dd
Suporte <suporte@devcyber.com>
2026-08-30 23:09:01 -0300

    nome do vinculo em despesas

--- corpo/views.py
+++ corpo/views.py
@@ -153,7 +153,7 @@ def filtro_despesa(request):
     localizar_ip = LocationService()
     resultado = localizar_ip.get_location(str(request.META.get('REMOTE_ADDR')))
     Logs(usuario=request.user, data_hora=datetime.now(), dados_post='', dados_pc_acesso=request.META.get('HTTP_USER_AGENT', ''), ip=request.META.get('REMOTE_ADDR'), tipo_acesso=request.method, pagina=request.path, endereco=resultado).save()
-    return render(request, 'filtro_despesa.html', {'permissao': request.user.last_name})
+    return render(request, 'filtro_despesas.html', {'permissao': request.user.last_name})
 
 @login_required
 def despesas_contrato(request, cod):
